a2b53c3db64834c89d6fef222094f5072d9304e2
KimTaeHa <70803824+xogk1128@users.noreply.github.com>
2024-07-21 00:03:48 +0900

    🚩 Feature#3 test2

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -33,6 +33,7 @@ function App() {
                 Click on the Vite and React logos to learn more!!!
             </p>
             <p className="read-the-docs">test1</p>
+            <p className="read-the-docs">test2</p>
         </>
     );
 }
